e88e5c6556ba78cfcc5ac504f6c019aba69655f1
andrewy <andrew.yamane@gmail.com>
2018-05-14 06:33:20 -1000

    Intial commit

---
+++ server.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const express = require('express');   // expressjs.com
+const hbs = require('hbs');   // handlebarsjs.com, npmjs.com/package/hbs
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+// Uncomment to put website into maintenance mode
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs', {
+//     pageTitle: 'Maintenance Page'
+//   });
+// });
+
+app.use(express.static(__dirname + '/public'));   // register the express middleware
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to the website'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to fulfill request'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
